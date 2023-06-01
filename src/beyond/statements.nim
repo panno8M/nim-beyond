8304954e6723af87af464d326a4c141b989da06d
la .panon <panno.800k@gmail.com>
2023-06-01 14:24:27 +0900

    Make the comments feature of statements more flexible.

--- src/beyond/statements.nim
+++ src/beyond/statements.nim
@@ -1,21 +1,32 @@
 import std/[
-  strformat,
   strutils,
+  sequtils,
+  options,
 ]
 
 const
   siSentence = 0
   siHeader = 2
+  siPadding = 0
 type
   StmtKind* {.pure.} = enum
     skSentence
     skDummy
+  StmtCommentStat* {.pure.} = enum
+    scsPlain
+    scsDocComment
+    scsComment
   Statement* = ref object
     kind* = skSentence
     content*: string
-    commentedout*: bool
+    commentStat*: StmtCommentStat
     children*: seq[Statement]
     childIndent* = siHeader
+    paddingIndent* = siPadding
+  ConfigStringify* = object
+    ignoreComment*: bool
+    ignoreDocComment*: bool
+
 
 func sentence*(_: typedesc[Statement]; content: string): Statement =
   Statement(
@@ -44,25 +55,50 @@ func add*(stmt: Statement; children: varargs[Statement]): Statement {.discardabl
   stmt.children.add children
   return stmt
 
-func commentout*(stmt: Statement; `y/n` = true): Statement {.discardable.} =
-  stmt.commentedout = `y/n`
-  return stmt
-func indentChildren*(stmt: Statement; childIndent: int; `y/n` = true): Statement {.discardable.} =
+func asComment*(stmt: Statement; `y/n` = true): Statement {.discardable.} =
   if `y/n`:
-    stmt.childIndent = childIndent
+    stmt.commentStat = scsComment
+  return stmt
+func asDocComment*(stmt: Statement; `y/n` = true): Statement {.discardable.} =
+  if `y/n`:
+    stmt.commentStat = scsDocComment
+  return stmt
+func indentChildren*(stmt: Statement; lvIndent: int; `y/n` = true): Statement {.discardable.} =
+  if `y/n`:
+    stmt.childIndent = lvIndent
+  return stmt
+func indentSelf*(stmt: Statement; lvIndent: int; `y/n` = true): Statement {.discardable.} =
+  if `y/n`:
+    stmt.paddingIndent = lvIndent
   return stmt
 
-func commentout(str: string; `y/n` = true): string =
-  if `y/n`: &"# {str}"
-  else: str
+func applyComment(str: string; stat: StmtCommentStat; cfg: ConfigStringify): Option[string] =
+  case stat
+  of scsPlain:
+    some str
+  of scsDocComment:
+    if cfg.ignoreDocComment:
+      none string
+    else:
+      some str.splitLines.mapIt("## "&it).join("\n")
+  of scsComment:
+    if cfg.ignoreComment:
+      none string
+    else:
+      some str.splitLines.mapIt("# "&it).join("\n")
 
-func stringify*(stmt: Statement, indent: Natural): string =
+func stringify*(stmt: Statement; cfg: ConfigStringify; indent: Natural = 0): string =
+  let idtSlf = indent+stmt.paddingIndent
+  let idtCld = idtSlf+stmt.childIndent
   case stmt.kind
   of skSentence:
-    result.add stmt.content.commentout(stmt.commentedout).indent(indent) & '\n'
+    let commented = stmt.content.applyComment(stmt.commentStat, cfg)
+    if commented.isSome:
+      result.add (get commented).indent(idtSlf) & '\n'
   of skDummy:
     discard
   for child in stmt.children:
-    result.add child.stringify(indent+stmt.childIndent)
+    if child.isNil: continue
+    result.add child.stringify(cfg, idtCld)
 
-template `$`*(stmt: Statement): string = stmt.stringify(0)
+template `$`*(stmt: Statement): string = stmt.stringify(ConfigStringify())
